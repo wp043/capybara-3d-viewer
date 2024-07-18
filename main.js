70870b7b198a21f6eb0d0d5917fdc9ee4335533c
wp043 <22.wendy.pan@gmail.com>
2024-07-18 08:42:26 +0800

    Revert "page view"
    This reverts commit 5bdf5ba1ab9055ca383c9d005bdc0f03791f4a48.

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import gsap from 'gsap';
 
-import { Analytics } from "@vercel/analytics/react"
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas') });
